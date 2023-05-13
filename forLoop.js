314ec4e1dd52a95dfa81f87e68b96cfcc2c30a8c
Raja Jeff Edward Pariñas Marimuthu <133474900+Jeff030197@users.noreply.github.com>
2023-05-13 19:27:58 +0800

    forLoop

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+const classNames = ["Kieth Jolo Solimen Nagutom", "Hunderson Fabelon Falogme", "Caezzy Jhonn Bongcayao", "Jerome Marquez Legaspi","Thrisha Janine Tagudando Caballero", "Sim Lloyd Lomongsod Separes", "Jerry Joy Romano Ismael", "Ian Gabriel Ceballo Noble","John Eric Manalang Yuzon", "Jomell Hope Garcia Dela Pena", "Eldrich jyuri Sumabat", 
+"Kevin Dangayo Atiwon" , "dave ryan enate catimbang", "Joven Motul Ison","Raja Jeff Edward Parinas Marimuthu", "Lawrence Banta Padrinao", "Mark Anthony Espanola Carino"]
+
+const myName = 'Raja Jeff Edward Parinas Marimuthu';
+
+for (let name = 0; name < classNames.length; name++) {
+  console.log(classNames[name]);
+  if (myName === classNames[name]) {
+    break;
+  }
+}
